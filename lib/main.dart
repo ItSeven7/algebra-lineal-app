05d68b9adfa61bec709c6bc83f7bf8349f7e3b3c
Ari Rodriguez <ari19.lucer@gmail.com>
2025-09-26 23:13:44 -0600

    Versión compatible con Android

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await Firebase.initializeApp();
 
   final themeUser = await UserService().obtenerTemaDesdeFirestore();
 
