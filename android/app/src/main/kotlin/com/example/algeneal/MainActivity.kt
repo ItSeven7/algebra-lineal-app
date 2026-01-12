43da50754816ce873a1c6f172687cbc9243f3391
Ari Rodriguez <ari19.lucer@gmail.com>
2026-01-11 19:26:14 -0600

    Datos de compilación de aplicación actualizados

---
+++ android/app/src/main/kotlin/com/example/algeneal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.algeneal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/aplication_algebra_lineal/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aplication_algebra_lineal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
