e7d21ec7b2044d90b455c130c0d5e2dbfb552de6
Ari Rodriguez <ari19.lucer@gmail.com>
2026-02-26 19:45:23 -0600

    Versionado en la pantalla about

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -59,7 +59,7 @@ class AboutPage extends StatelessWidget {
       ),
       bottomSheet: const Padding(
         padding: EdgeInsets.only(bottom: 20),
-        child: Text('Versión: 2.0.0',
+        child: Text('Versión: 2.1.1',
             textAlign: TextAlign.center,
             style: AppTextStyles.bodyBlack,
             overflow: TextOverflow.visible),
